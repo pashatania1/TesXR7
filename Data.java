9db9da0364e1919befea1704da1899fc69456d9f
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2024-08-05 11:37:25 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "Pasha";
+        int usia = 17;
+        String alamat = "Jl. Jaya Srani Sawojajar";
+        double kelas = 10.5;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Usia : " + usia + " tahun");
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
